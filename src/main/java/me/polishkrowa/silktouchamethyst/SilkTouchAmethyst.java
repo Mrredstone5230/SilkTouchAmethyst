187175bc4e4e74d6b0c1d7a56c304f9c6e6684b1
Mr_redstone5230 <sebastienduf@icloud.com>
2021-07-01 13:54:32 -0400

    Initial Commit

---
+++ src/main/java/me/polishkrowa/silktouchamethyst/SilkTouchAmethyst.java
@@ -0,0 +1,44 @@
+package me.polishkrowa.silktouchamethyst;
+
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public final class SilkTouchAmethyst extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() { }
+
+    @EventHandler
+    public static void onBlockBreak(BlockBreakEvent event) {
+        if (!event.getBlock().getType().equals(Material.BUDDING_AMETHYST))
+            return;
+        if (event.getPlayer().getGameMode().equals(GameMode.CREATIVE))
+            return;
+        if (event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.AIR))
+            return;
+
+        if (!event.getPlayer().getInventory().getItemInMainHand().getEnchantments().containsKey(Enchantment.SILK_TOUCH))
+            return;
+
+        event.setExpToDrop(0);
+        event.setDropItems(false);
+        Location loc = event.getBlock().getLocation();
+        World world = event.getBlock().getWorld();
+
+        world.dropItemNaturally(loc, new ItemStack(Material.BUDDING_AMETHYST));
+    }
+}
